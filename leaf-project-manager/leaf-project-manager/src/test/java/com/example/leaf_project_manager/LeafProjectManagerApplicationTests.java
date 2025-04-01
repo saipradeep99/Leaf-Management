60545fbdf44002c779cea13410f5a38437fa5a83
Nelluri Venkata Siva Sai Pradeep <38180417+saipradeep99@users.noreply.github.com>
2025-04-01 17:50:05 +0000

    Initial Commit

---
+++ leaf-project-manager/leaf-project-manager/src/test/java/com/example/leaf_project_manager/LeafProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaf_project_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
